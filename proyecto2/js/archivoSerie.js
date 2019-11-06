039862e28d9d3c57a3425ef7a5137a00252c9357
Bernardita Chacon <bernarditachacon.bc@gmail.com>
2019-11-05 22:09:23 -0300

    acomode la parte mobile

--- proyecto2/js/archivoSerie.js
+++ proyecto2/js/archivoSerie.js
@@ -1,30 +1,4 @@
 
-//FUNCIONES PARA QUE APAREZCAN Y DESAPAREZCAN LOS MENUES
-
-let notif = document.querySelector('.notificaciones');
-notif.hidden = true;
-
-let user = document.querySelector('.menuUsuario');
-user.hidden = true;
-
-function mostrarNotificaciones(){
-    notif.hidden = false;
-    notif.addEventListener('mouseleave', esconderNotificacion);
-}
-
-function esconderNotificacion(){
-    notif.hidden = true;
-}
-
-function mostrarMenuUser(){
-    user.hidden = false;
-    user.addEventListener('mouseleave', esconderMenuUser);
-}
-
-function esconderMenuUser(){
-    user.hidden = true;
-}
-
 //FUNCION PARA QUE DESAPAREZCA LA ESTRELLITA VACIA Y APAREZCA LA LLENA
 let estrellaVacia = document.querySelector('.iconoEstrellaVacia');
 estrellaVacia.hidden = false;
